b4a4b75060d35b67a218cfa621f05f741f9dc765
crisarau <araujo.j.cristian@gmail.com>
2022-07-29 04:18:31 -0400

    autocomplete search

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,11 +1,14 @@
 import "./Modal.scss";
 
 import ReactDOM from "react-dom"; 
-
+import useStyles from './styles.js';
 import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 
-const Modal = ({isOpen, closer})=> {
+const Modal = ({isOpen, closer, onPlaceChanged, onLoad})=> {
 
+    const classes = useStyles();
     let deleteHandler = async ()=>{
         //let deleteReponse = await delete()
         //console.log("deleting: "  + props.inventorySelected.itemName);
@@ -19,9 +22,14 @@ const Modal = ({isOpen, closer})=> {
             (
                 <> 
                     <button onClick={closer}>close</button>
-                    {/* <Autocomplete>
-                        
-                    </Autocomplete> */}
+                    <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+                        </div>
+                    </Autocomplete>
                 </>
             ):(null)
         }
--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -4,15 +4,31 @@ import Modal from '../../components/Modal/Modal';
 
 
 function HomePage() {
-
+  const [coords, setCoords] = useState({});
   const [isOpen, setIsOpen] = useState(false);
+  const [autocomplete, setAutocomplete] = useState(null);
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onLoad = (autoC) => setAutocomplete(autoC);
+
+  const onPlaceChanged = () => {
+    console.log(autocomplete);
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+
+    setCoords({ lat, lng });
+  };
+
 
   return (
-    <div className="App">
+    <>
+      <div className="App">
       <button onClick={()=>setIsOpen(true)}> OpenModal</button>
-
-      <Modal isOpen={isOpen} closer={()=>setIsOpen(false)}/>
-    </div>
+      </div>
+      <Modal isOpen={isOpen} closer={()=>setIsOpen(false)} onPlaceChanged={onPlaceChanged} onLoad={onLoad}/>
+    </>
+    
   );
 }
 
