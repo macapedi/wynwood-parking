7507711a7c54867eacf6743354aa5986b4457de9
macapedi <macapedi90@gmail.com>
2022-07-29 12:52:08 -0400

    final styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,6 +12,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import React from 'react';
+import freeguy from "../../assets/images/freeguy.png";
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -128,7 +129,7 @@ const ResponsiveAppBar = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Remy Sharp" src={freeguy} />
               </IconButton>
             </Tooltip>
             <Menu
