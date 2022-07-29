d10e6a47fa1844e2fe383b9d3ce4ec7c6ca2b9c9
crisarau <araujo.j.cristian@gmail.com>
2022-07-29 05:28:00 -0400

    it's something

--- src/pages/MapPage/MapPage.js
+++ src/pages/MapPage/MapPage.js
@@ -1,6 +1,9 @@
 
 import './MapPage.scss';
 import GoogleMapReact from 'google-map-react';
+import data from '../../data/data.json';
+import React, { useState } from 'react';
+
 
 const center = {
   lat: 25.799904,
@@ -10,10 +13,11 @@ const center = {
 const MapPage= () => {
 
   const searchParams = new URLSearchParams(window.location.search);
-  for (const [key, value] of searchParams) {
-    console.log({ key, value }) // {key: 'term', value: 'pizza'} {key: 'location', value: 'Bangalore'}
-  }
+  // for (const [key, value] of searchParams) {
+  //   console.log({ key, value }) // {key: 'term', value: 'pizza'} {key: 'location', value: 'Bangalore'}
+  // }
 
+  const [childClicked, setChildClicked] = useState(null);
 
   return (
     <div className="App" style={{ height: '100vh', width: '100%' }}>
@@ -24,9 +28,22 @@ const MapPage= () => {
         center={center}
         defaultZoom={15}
         margin={[50,50,50,50]}
+        onChildClick={(child) => {
+          console.log(child);
+          setChildClicked(child);}}
         >
-          
+          {data.map((place,i)=>{
+            //console.log(place);
+            return <div className='marker' 
+            lat={place.latitude}
+            lng={place.longitude}
+            key={i}
+            />
+          })}
+
         </GoogleMapReact>
+
+      <div>{childClicked ? (data[childClicked].latitude + " " + data[childClicked].longitude):("nothing selected")}</div>
     </div>
   );
 }
